a5f28ec4d9efd6aa42b3cb59116b47e9929dc02e
luzac <corialuzantonella@gmail.com>
2025-12-05 11:11:37 -0300

    Actualización del juego y estilos

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\aldito\Documents\GitHub\SoftwareEducativo\venv"
+set "VIRTUAL_ENV=C:\Users\Luz Coria\Desktop\SoftwareEducativo\juegoeducativo\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
